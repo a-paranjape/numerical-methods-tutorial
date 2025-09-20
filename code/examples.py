01b40afa7f3ef162cea301067403d1d006c188a4
a-paranjape <aseem@iucaa.in>
2025-09-20 11:34:13 +0530

    reorganized using classes, so that exact solutions can be uniformly handled

--- code/examples.py
+++ code/examples.py
@@ -1,5 +1,9 @@
 import numpy as np
 
 # Simple harmonic oscillator
-def f_ut_SHO(u,t,theta_f):
-    return -theta_f[0]**2*u
+class SimpleHarmonicOscillator(object):
+    def f_ut(self,u,t,theta_f):
+        return -theta_f[0]**2*u
+
+    def u_exact(self,t,u0,v0,theta_f):
+        return u0*np.cos(theta_f[0]*t) + (v0/theta_f[0])*np.sin(theta_f[0]*t)
